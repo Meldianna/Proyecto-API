d1adf1dc8f2bf2df4e974b4f7253f9fe94263e97
Melanie Diana Rodríguez <melaniediana@Melanies-MacBook-Pro.local>
2025-08-28 17:18:55 -0300

    casi final

--- src/main/java/com/uade/tpo/demo/controllers/ProductsContoller.java
+++ src/main/java/com/uade/tpo/demo/controllers/ProductsContoller.java
@@ -134,7 +134,10 @@ public class ProductsContoller {
      @DeleteMapping("/delete/{productId}")
      public ResponseEntity<Object> deleteProduct(@PathVariable Long productId) {
 
+
+        
         productService.deleteProduct(productId);
+
         return ResponseEntity.noContent().build();
      }
      
