d9b2ab1ae7fa9480301a49ece376f4fec088d302
meganjac <meganr2609@gmail.com>
2025-09-06 15:54:13 -0300

    new version

--- src/main/java/com/uade/tpo/demo/repository/PaymentMethodRepository.java
+++ src/main/java/com/uade/tpo/demo/repository/PaymentMethodRepository.java
@@ -1,17 +1,13 @@
 package com.uade.tpo.demo.repository;
 
-
-
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.data.jpa.repository.Query;
+
 import com.uade.tpo.demo.entity.PaymentMethod;
 
 public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Long> {
     
     @Query(value = "select p from PaymentMethod p where p.description = ?1")
     PaymentMethod findByDescription(String description);
-
 
 }
